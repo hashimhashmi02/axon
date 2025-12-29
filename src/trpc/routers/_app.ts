1a3e77ed75aab8322f46716e7c716caf428d04ee
hashimhashmi02 <hashimhashmi02@gmail.com>
2025-12-29 20:26:08 +0530

    AI-providers

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,7 +1,19 @@
 import { inngest } from '@/inngest/client';
 import { createTRPCRouter, protectedProcedure } from '../init';
 import prisma from '@/lib/db';
+import { google } from '@ai-sdk/google';
+import { generateText } from 'ai';
+
+
 export const appRouter = createTRPCRouter({
+  testAi: protectedProcedure.mutation(async()=>{
+    await inngest.send({
+      
+      name:"execute/ai",
+    })
+return {success: true, message :"Job Queued"}
+  }),
+
   getWorkflow: protectedProcedure.query(({ctx})=>{
     return prisma.workflow.findMany();
   }),
