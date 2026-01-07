d9e091d510f069a96a0fce02f6dd63b36dd4d8a8
hashimhashmi02 <hashimhashmi02@gmail.com>
2026-01-08 02:07:37 +0530

    node-selector

---
+++ src/features/triggers/components/manual-trigger/node.tsx
@@ -0,0 +1,23 @@
+import { NodeProps } from "@xyflow/react";
+import { memo } from "react";
+import { BaseTriggerNode } from "../base-trigger-node";
+import { MousePointerIcon } from "lucide-react";
+
+
+export const ManualTriggerNode = memo((props: NodeProps) => {
+    return (
+        <>
+            <BaseTriggerNode
+                {...props}
+                icon={MousePointerIcon}
+                name="When clicking 'Execute workflow'"
+                description="Manually trigger this workflow"
+
+
+
+
+            />
+
+        </>
+    )
+})
